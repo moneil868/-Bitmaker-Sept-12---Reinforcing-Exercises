f26998be91f7db63466a78664dffe041932733cb
Marlon O'Neil <hello@marlononeil.com>
2017-09-20 18:22:17 -0400

    Completed exercise 1

--- exercise.rb
+++ exercise.rb
@@ -1,4 +1,5 @@
 def check_syntax(str)
+
   openables = {
     "(" => ")",
     "[" => "]",
@@ -10,31 +11,29 @@ def check_syntax(str)
 
   str.each_char do |char|
 
-    if opening_chars.include?(char) # opening bracket
 
+    if opening_chars.include?(char) # opening bracket
       open << char
 
     elsif closing_chars.include?(char) # closing bracket
-
       required_char = openables[open.last]
 
       if char == required_char # it's the right kind of closing bracket
-
         open.pop
-
       else
 
         if open.any? # it's the wrong kind of closing bracket
-          puts "* You have a syntax error: there is an unexpected #{char}"
+          puts "* You have a syntax error: there is an unexpected #{char} where there should be a #{required_char}"
         else # there's nothing to close
           puts "* You have a syntax error: there is an unexpected #{char} where there is nothing to close."
         end
 
         return false
-
       end
 
     end
+
+
   end
 
   if open.any?
